5bd65c9dbc362babcc8705e72639a7839c6e8071
ebront08 <ebront08@gmail.com>
2021-12-13 09:01:57 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Employee Management Systems",
+    subtitle: "SQL",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./employeemanagement.gif",
+    link: "https://github.com/ebront08/employeemanagementsystem",
+  },
+  {
+    title: "Fitness Tracker",
+    subtitle: "Nosql",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "fitnesstracker.gif",
+    link: "https://github.com/ebront08/fitnesstracker",
+  },
+  {
+    title: "Notetaker",
+    subtitle: "Express.js",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./notetaker.gif",
+    link: "https://github.com/ebront08/notetaker",
+  },
+  {
+    title: "Resume",
+    subtitle: "FullStack Resume",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./RESUME.gif",
+    link: "https://drive.google.com/file/d/1GBd-yeH-6Rdz76z2GH2-AXVQgP4OTWut/view?usp=sharing",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node.js",
+  "NoSQL",
+  "GraphQL",
+  "MERN",
+];
